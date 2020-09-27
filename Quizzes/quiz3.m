c4d582c4101512d06f4bc1385c27550f7648abb2
ajalsingh <ajal.singh@multifin.com.au>
2020-09-28 01:37:14 +1000

    quize scripts

--- Quizzes/quiz3.m
+++ Quizzes/quiz3.m
@@ -10,7 +10,7 @@ L4 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]);
 L5 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi]);
 
 robot = SerialLink([L1 L2 L3 L4 L5],'name','myRobot');                     
-q = deg2rad([30 -60 45 -30 0]);                                                     % Create a vector of initial joint angles        
+q = deg2rad([30,-30,35,-30,0]);                                                     % Create a vector of initial joint angles        
 
 scale = 0.5;
 workspace = [-2 2 -2 2 -0.05 2];                                       % Set the size of the workspace when drawing the robot
@@ -23,18 +23,18 @@ close all;
 mdl_puma560;
 
 % point provided
-otherPoint = [0.6304 -2.149 0.6223];
+otherPoint = [1 -2.260 1];
 
 % adjust joint states
-q = deg2rad([0, 45, -80, 0, 45, 0]);
+q = deg2rad([0, 40, -80, 0, 45, 0]);
 T = p560.fkine(q);
 distance = norm(T(1:3,4)' - otherPoint)
-%%
+
 p560.plot(q);
 view(3);
 hold on;
 
-r = 1 ;
+r = 2.2 ;
 [x,y,z] = sphere;
 surf(r*x+T(1,4),r*y+T(2,4),r*z+T(1,4));
 axis equal
@@ -43,8 +43,9 @@ for i=-2.5:0.01:2.5
     scatter3(1,i,1,'filled')
 end
 %%
-for i=-2.5:0.00001:2
-    distance = norm(T(1:3,4)' - [1 i 1]);
+clc;
+for i=-2.284:0.00001:-2.260
+    distance = norm(T(1:3,4)' - [1 i 1])
     if round(distance,3) == r
         ans = i
         break;
@@ -59,7 +60,7 @@ close all;
 q1= [pi/10, pi/7, pi/5, pi/3, pi/4, pi/6];
 q2 = [-pi/6, -pi/3, -pi/4, -pi/8, -pi/7, -pi/10];
 
-steps = 35;
+steps = 65;
 
 interpolation = 2;
 
@@ -94,7 +95,7 @@ mdl_puma560;
 
 % chnage the following:
 p560.base = transl(0,0,0.0);
-q = deg2rad([90, 30, -80, 0, 45, 0]);
+q = deg2rad([-90, 30, -80, 0, 50, 0]);
 ball = transl(0.4,-0.2,0.7);               %target location in global frame
 %
 
@@ -113,7 +114,7 @@ clc;
 close all;
 
 mdl_puma560;
-T = transl([0.6 0.1 0.1]);
+T = transl([0.5,0.2,0.4]);
 p560.ikine(T,qn, [1 1 1 0 0 0])
 
 
@@ -156,17 +157,17 @@ robot.plot(q,'workspace',workspace,'scale',scale);                  % Plot the r
 %Create a puma 560.
 mdl_puma560;
 %The robot is at q = [pi/20,0,-pi/2,0,0,0].
-q = [pi/12,0,-pi/2,0,0,0]%Q INPUT
+q = [pi/6,0,-pi/2,0,0,0];%Q INPUT
 
 %Determine where a ray cast from the Z axis (the approach vector) of the end effector intersects with a planar wall. (i.e. normal = [-1 0 0], point = [1.2 0 0]).
-endEffectorTr = p560.fkine(q)
+endEffectorTr = p560.fkine(q);
 p560.teach(q)
 
 hold on
 
 normal = [-1,0,0] ;%Q INPUT
-point= [1.8,0,0] ;%Q INPUT
-planeXntersect = 1.8;%Q INPUT
+point= [3.6,0,0] ;%Q INPUT
+planeXntersect = 3.6;%Q INPUT
 
 planeBounds = [planeXntersect-eps,planeXntersect+eps,-2,2,-2,2]; 
 [Y,Z] = meshgrid(planeBounds(3):0.1:planeBounds(4),planeBounds(5):0.1:planeBounds(6));
@@ -179,8 +180,25 @@ point2OnLine = rayEndTr(1:3,4)';
 
 [intersectionPoint,check] = LinePlaneIntersection(normal,point,point1OnLine,point2OnLine)
 %% 3 link plane
+close all;
+clear;
+clc;
+
 mdl_3link3d;
-q = [-pi/6,0,0];
-normal = [3.1,0,0];
-% [intersectP,check] = LinePlaneIntersection(normal,point,tr(1:3,4,i)',tr(1:3,4,i+1)');
-R3.plot(q)
+q = [-pi/6,0,0]
+endEffectorTr = R3.fkine(q)
+R3.teach(q)
+
+hold on
+planeXntersect = 3;
+planeBounds = [planeXntersect-eps,planeXntersect+eps,-3,3,-3,3]; 
+[Y,Z] = meshgrid(planeBounds(3):0.1:planeBounds(4),planeBounds(5):0.1:planeBounds(6));
+X = repmat(planeXntersect,size(Y,1),size(Y,2));
+surf(X,Y,Z);
+
+point1OnLine = endEffectorTr(1:3,4)';
+point2OnLine = [0 0 1];
+normal = [-3 0 0]
+point = [3   0 0]
+[intersectionPoint,check] = LinePlaneIntersection(...
+    normal,point,point1OnLine,point2OnLine)
